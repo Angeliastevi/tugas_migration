778f9e1f2779d50fa9e6d28b5fb1517e55fa6302
AngeliaStevinesyabudiL <angelia_stevinesyabudi_27rpl@student.smktelkom-mlg.sch.id>
2020-01-14 14:34:23 +0700

    commit satu

---
+++ database/migrations/2020_01_14_060231_create_table_peminjaman.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePeminjaman extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_anggota');
+            $table->integer('id_petugas');
+            $table->integer('tgl_pinjam');
+            $table->integer('deadline');
+            $table->integer('tgl_kembali');
+            $table->integer('denda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+}
